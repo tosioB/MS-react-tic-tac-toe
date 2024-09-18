7266a4c5f9834ebfda45e5af19fbf0ab916e9e1f
kimdaesik <ds500033@naver.com>
2024-09-18 18:55:01 +0900

    tic-tac-toe 프로젝트 따라하기

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,24 @@
+function GameBoard({ onSlectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => onSlectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default GameBoard;
